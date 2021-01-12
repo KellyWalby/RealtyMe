5f881c07a45f47e4aa9419f43a62c2b3dfe3fe96
Kelly Walby <walbs18@gmail.com>
2021-01-12 17:25:43 -0600

    test

---
+++ RealtyMe/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RealtyMe
+//
+//  Created by Kelly Walby on 1/12/21.
+//  Copyright © 2021 Kelly Walby. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class ViewController: UIViewController {
+    var ref: DatabaseReference!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = Database.database().reference()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
