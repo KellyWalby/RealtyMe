48193bcbe9d8c8a8777fe5b37d3b816dde39bcd1
Kelly Walby <walbs18@gmail.com>
2021-02-25 23:26:55 -0600

    collectionView home screen (scrollable), not connected w/ db

--- RealtyMe/ViewControllers/HomeViewController.swift
+++ RealtyMe/ViewControllers/HomeViewController.swift
@@ -7,33 +7,77 @@
 //
 
 import UIKit
+import Firebase
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     
+    let db = Firestore.firestore()
+    var count = 0
+    var num = 20
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width:collectionView.frame.width/2.1, height: collectionView.frame.width/2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+// ????? TRIED TO COUNT NUMBER OF LISTINGS SO TILES WOULD REFLECT THAT BUT IT DIDNT WORK
+//        db.collection("listings").getDocuments()
+//        {
+//            (querySnapshot, err) in
+//            if let err = err
+//            {
+//                print("Error getting documents: \(err)");
+//            }
+//            else
+//            {
+//                for document in querySnapshot!.documents {
+//                    self.count += 1
+//                }
+//                self.num = self.count
+//            }
+//        }
+        return num //probs change to 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+        cell.backgroundColor = .gray //background color of cell
+        cell.layer.cornerRadius = 8 //adds rounded corner to tiles
+        return cell
+    }
     
     @IBOutlet weak var homeToolbarButton: UIBarButtonItem!
     @IBOutlet weak var messageToolbarButton: UIBarButtonItem!
     @IBOutlet weak var notificationToolbarButton: UIBarButtonItem!
     @IBOutlet weak var accountToolbarButton: UIBarButtonItem!
     
+    fileprivate let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+
+        return cv
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //setting up collection view colors & constraints
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .white
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        collectionView.showsVerticalScrollIndicator = false //makes scroll bar invisable
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
-    
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     
     @IBAction func homeToolbarButtonTapped(_ sender: Any) {
@@ -53,4 +97,30 @@ class HomeViewController: UIViewController {
         self.performSegue(withIdentifier: "HomeToAccountSegue", sender: nil)
     }
     
+    
+}
+
+class CustomCell: UICollectionViewCell{
+    
+    fileprivate let bg: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        contentView.addSubview(bg)
+        bg.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        bg.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        bg.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        bg.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
