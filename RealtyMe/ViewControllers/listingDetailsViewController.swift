75afe96a6edd8703badfe58dbc80585a3a5d1c04
Kelly Walby <Walbs18@gmail.com>
2021-03-24 14:09:47 -0500

    UI Design for Listing Details with Image placed in code for general idea. Still have to figure out to pull information from the corresponding collectionview cell to populate with correct data.

--- RealtyMe/ViewControllers/listingDetailsViewController.swift
+++ RealtyMe/ViewControllers/listingDetailsViewController.swift
@@ -12,15 +12,52 @@ class listingDetailsViewController: UIViewController {
     
     
     @IBOutlet weak var homeToolbarButton: UIBarButtonItem!
-    
     @IBOutlet weak var messageToolbarButton: UIBarButtonItem!
-    
     @IBOutlet weak var notificationToolbarButton: UIBarButtonItem!
-    
     @IBOutlet weak var accountToolbarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var listingImage: UIImageView!
+    
+    @IBOutlet weak var listingStreetName: UILabel!
+    
+    @IBOutlet weak var listingAddress: UILabel!
+    @IBOutlet weak var listingPrice: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var listingSqFt: UILabel!
+    
+    @IBOutlet weak var bookmark: UIButton!
+    
+    @IBOutlet weak var listingFeatures: UILabel!
+    
+    @IBOutlet weak var listingDescription: UITextView!
+    
+    @IBOutlet weak var bedrooms: UILabel!
+    
+    @IBOutlet weak var bathrooms: UILabel!
+    
+    @IBOutlet weak var yearBuilt: UILabel!
+    
+    @IBOutlet weak var extraFeature1: UILabel!
+    
+    @IBOutlet weak var extraFeature2: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        listingImage.image = UIImage(named: "IMG_5797")
+//        listingStreetName.text = "2685 Riviera Dr. S."
+//        listingAddress.text = "White Bear Lake, MN, 55110"
+//        listingPrice.text = "$600,000"
+//        listingSqFt.text = "4,000"
+//        listingDescription.text = "Great place to raise a family!"
+//        bedrooms.text = "Bedrooms: 3"
+//        bathrooms.text = "Bathrooms: 3"
+//        yearBuilt.text = "1983"
+//        extraFeature1.text = "Built-in Pool"
+//        extraFeature2.text = "Home Movie Theater"
+        
+        listingDescription.isEditable = false
 
         // Do any additional setup after loading the view.
     }
